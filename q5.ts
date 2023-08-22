e6e010d476da5baeddb5ace8a46291bf676388df
razie <r.jedari@alibaba.com>
2023-08-22 17:38:51 +0330

    problem five  solved

---
+++ q5.ts
@@ -0,0 +1,17 @@
+function countVowels(value: string) {
+    let x = value.split("");
+  
+    const vowels = ["a", "e", "i", "o", "u"];
+  
+    let count = 0;
+  
+    for (let i = 0; i < value.length; i++) {
+      if (vowels.includes(value[i])) {
+        count++;
+      }
+    }
+  
+    return count;
+  }
+  
+  countVowels("hello world!");
