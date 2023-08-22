6083aeeebe20f82ba84fe10a45ad7e53503d0667
amir Rashid <49517185+amRashidi@users.noreply.github.com>
2023-08-22 18:10:42 +0330

    fix: out put directory

--- q1.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function isPalindrome(str) {
-    let re = /[\W_]/g;
-    let lowRegStr = str.toLowerCase().replace(re, "");
-    let reverseStr = lowRegStr.split("").reverse().join("");
-    return reverseStr === lowRegStr;
-}
-const x = isPalindrome("Cigar? Toss it in a can. It is so tragic");
-const y = isPalindrome("sit ad est love");
